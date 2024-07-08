ea60549c411a2ccd6526a439e917fa0dceb63035
X. Michalet <michalet@chem.ucla.edu>
2024-07-08 15:45:33 -0700

    AlliGator 0.90 release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,10 +47,10 @@ copyright = '2015-2024, Regents of the University of California'
 author = 'X. Michalet'
 
 # The short X.Y version.
-version = '0.89.1'
+version = '0.90'
 
 # The full version, including alpha/beta/rc tags
-release = '0.89.1'
+release = '0.90'
 
 
 # -- General configuration ---------------------------------------------------
